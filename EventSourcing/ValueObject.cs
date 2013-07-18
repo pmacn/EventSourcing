6a75262a5d9a876e971560c78defade1089ae0d1
pmacnaughton <peter@macnaughton.info>
2013-07-18 10:24:41 -0600

    Added ValueObject abstract class
    The ValueObject abstract class is an attempt to simplify implementing Equality
    and GetHashCode for value objects.

---
+++ EventSourcing/ValueObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventSourcing
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        protected abstract IEnumerable<object> GetDefiningValues();
+
+        public override bool Equals(object obj)
+        {
+            if (Object.ReferenceEquals(obj, null)) return false;
+            if (Object.ReferenceEquals(this, obj)) return true;
+            return Equals(obj as ValueObject);
+        }
+
+        public bool Equals(ValueObject other)
+        {
+            if(other == null)
+                return false;
+
+            var thisValuesEnumerator = GetDefiningValues().GetEnumerator();
+            var thatValuesEnumerator = other.GetDefiningValues().GetEnumerator();
+            while (thisValuesEnumerator.MoveNext() && thatValuesEnumerator.MoveNext())
+            {
+                if(!thisValuesEnumerator.Current.Equals(thatValuesEnumerator.Current))
+                    return false;
+            }
+
+            return true;
+        }
+
+        // TODO: This hash algoritm is probably not good enough for ValueObject
+        public override int GetHashCode()
+        {
+            var values = GetDefiningValues();
+            int hash = 23;
+            unchecked
+            {
+                foreach (var value in values)
+                {
+                    hash = hash * 31 + values.GetHashCode();
+                }
+            }
+
+            return hash;
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
