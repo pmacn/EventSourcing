98325d496e7bb5bf2971299e68901dcd1a7b1296
pmacnaughton <peter@macnaughton.info>
2013-07-22 00:16:41 -0600

    Removed lingering reference
    Had a reference from my original project for this
    that I had missed to uninstall

--- EventSourcing/ApplicationService.cs
+++ EventSourcing/ApplicationService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace EventSourcing
+﻿namespace EventSourcing
 {
     public interface IApplicationService<TIdentity>
         where TIdentity : IIdentity
