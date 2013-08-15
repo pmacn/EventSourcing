8bb40aacec868b994bb5da4c2121d4c09737b92e
pmacnaughton <peter@macnaughton.info>
2013-08-15 11:21:33 -0600

    Added DelegateAggregateFactory
    Fixed reference in EventSourcing.Tests

--- src/EventSourcing/AggregateFactory.cs
+++ src/EventSourcing/AggregateFactory.cs
@@ -1,5 +1,6 @@
 ﻿using EventSourcing.Exceptions;
 using System;
+using System.Collections.Generic;
 
 namespace EventSourcing
 {
@@ -18,6 +19,29 @@ namespace EventSourcing
                 throw new AggregateConstructionException(String.Format("Could not find a default constructor for {0}", aggregateType.Name));
 
             return (TAggregate)ctor.Invoke(new object[0]);
+        }
+    }
+
+    
+    public class DelegateAggregateFactory : IAggregateFactory
+    {
+        private readonly Dictionary<Type, Func<object>> factoryMethods = new Dictionary<Type, Func<object>>();
+
+        public void Register<TAggregate>(Func<TAggregate> factoryMethod) where TAggregate : IAggregateRoot
+        {
+            var aggregateType = typeof(TAggregate);
+            factoryMethods.Add(aggregateType, () => factoryMethod());
+        }
+
+        public TAggregate Create<TAggregate>()
+            where TAggregate : IAggregateRoot
+        {
+            Func<object> factoryMethod;
+            var aggregateType = typeof(TAggregate);
+            if(!factoryMethods.TryGetValue(aggregateType, out factoryMethod))
+                throw new AggregateConstructionException("No registered factory method for aggregate of type " + aggregateType.Name);
+
+            return (TAggregate)factoryMethod();
         }
     }
 }
