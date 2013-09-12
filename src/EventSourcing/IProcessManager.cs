a365e298952fe689daad8bffc08153a6506f3802
pmacnaughton <peter@macnaughton.info>
2013-09-12 16:55:21 -0600

    Added the beginnings of an IProcessManager int..
    erface
    Need some work on application services next, they're not quite working
    as planned

---
+++ src/EventSourcing/IProcessManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing
+{
+    public interface IProcessManager
+    {
+        Guid Id { get; }
+        bool Completed { get; }
+        IEnumerable<ICommand> Commands { get; }
+    }
+}
